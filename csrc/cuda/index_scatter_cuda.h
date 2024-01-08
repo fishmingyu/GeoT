e4959aaf34b7182018bb7f6bd0256220043f3e6e
fishming <1661342068@qq.com>
2024-01-08 11:57:46 -0800

    [Debug] compilation

--- csrc/cuda/index_scatter_cuda.h
+++ csrc/cuda/index_scatter_cuda.h
@@ -4,4 +4,4 @@
 
 at::Tensor index_scatter_cuda(const int64_t dim, const at::Tensor &index,
                               const at::Tensor &src, const at::Tensor &dst,
-                              const c10::string_view reduce, const bool sorted)
+                              const c10::string_view reduce, const bool sorted);
