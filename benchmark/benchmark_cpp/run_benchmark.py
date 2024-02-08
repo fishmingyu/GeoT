b4b601899addc3a84f0dcabc508dc9cf682b4754
fishmingyu <1661342068@qq.com>
2024-02-07 22:10:40 -0800

    [Update] benchmark python

--- benchmark/benchmark_cpp/run_benchmark.py
+++ benchmark/benchmark_cpp/run_benchmark.py
@@ -3,9 +3,13 @@ import os
 # Run the benchmark
 # data dir, ../../data/idx_data
 data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../data/idx_data")
-# iterate over all files in the data dir   
-for file in os.listdir(data_dir):
+# iterate over all files in the data dir (follow the alphabetical order to run the benchmark)
+file_start = 0
+for file in sorted(os.listdir(data_dir)):
     # run the benchmark on each file
+    # print ith file, count the current number
+    print(f"Running benchmark on {file}, {file_start}th file of {len(os.listdir(data_dir))} files.")
+    file_start += 1
     for K in [1, 2, 4, 8, 16, 32, 64, 128, 256]:
         # print command
         # print(f"./build/benchmark {os.path.join(data_dir, file)} {K}")
