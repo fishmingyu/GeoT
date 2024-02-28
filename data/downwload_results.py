2f248305aa69d3cac95f8bac7f2555886b691b5b
fishmingyu <1661342068@qq.com>
2024-02-27 19:19:22 -0800

    [Update] download profiling results

---
+++ data/downwload_results.py
@@ -0,0 +1,10 @@
+import gdown
+
+output = "sr_result.csv"
+id = "1WUP6ix3jqhCpyW_3G_-Bqe7N_xKpOsB6"
+gdown.download(id=id, output=output)
+
+
+output = "pr_result.csv"
+id = "1artQdPO7Je7bS5xyamL05o2Bx2kKZUQi"
+gdown.download(id=id, output=output)
