a9d2baf639d45e3569b3e249bcbc4afb9fdda803
fishmingyu <1661342068@qq.com>
2024-03-08 22:03:18 -0800

    [Update] bench spmm

--- benchmark/bench_spmm.py
+++ benchmark/bench_spmm.py
@@ -3,6 +3,9 @@ import torch_index_scatter
 from utils import Dataset
 import time
 import torch_sparse
+from scipy.sparse import csr_matrix
+import dgsparse
+from dgsparse import spmm_sum
 
 def gather_scatter(src_index, dst_index, src, reduce):
     return torch_index_scatter.gather_scatter(src_index, dst_index, src, reduce)
@@ -13,6 +16,9 @@ def pytorch_spmm(A, B):
 def pyg_spmm(A, B, reduce):
     return torch_sparse.matmul(A, B, reduce)
 
+def dgsparse_spmm(A, B):
+    return spmm_sum(A, B, 0)
+
 def timeit(func, iter, *args, **kwargs):
     start = time.time()
     for _ in range(iter):
@@ -21,9 +27,16 @@ def timeit(func, iter, *args, **kwargs):
     end = time.time()
     # return each func's ms
     print(f"{func.__name__} time: {(end - start) / iter * 1000:.3f} ms")
+    return (end - start) / iter * 1000
 
 
-def test_gather_scatter(dataset, feature_size, device):
+def to_rowptr(row, size):
+    rowptr = torch.zeros(size + 1, dtype=torch.int64)
+    rowptr[1:] = torch.bincount(row, minlength=size)
+    rowptr = rowptr.cumsum(0)
+    return rowptr
+
+def test_gather_scatter(file, dataset, feature_size, device):
     g = Dataset(dataset, device)
     edge_index = g.edge_index
     sparse_size = g.size
@@ -36,21 +49,28 @@ def test_gather_scatter(dataset, feature_size, device):
     row = dst_index
     col = src_index
     value = torch.ones_like(row, dtype=torch.float32)
-    adj = torch.sparse_coo_tensor(torch.stack([row, col]), value, (sparse_size, sparse_size))
-    adj = adj.coalesce()
+    rowptr = to_rowptr(row, sparse_size).to(device)
+    # use rowptr, col, value to create sparse tensor for torch 
+    # dgsparse need int type for rowptr and col
+    adj = torch.sparse_csr_tensor(rowptr.int(), col.int(), value, (sparse_size, sparse_size))
+    dgsparse_adj = dgsparse.SparseTensor.from_torch_sparse_csr_tensor(
+            adj.detach(), True, requires_grad=False)
 
     # create sparse tensor for torch_sparse
     adj_torch_sparse = torch_sparse.SparseTensor(row=row, col=col, value=value, sparse_sizes=(sparse_size, sparse_size))
 
     # benchmark time
     iter = 100
+    # write to csv file
     timeit(gather_scatter, iter, src_index, dst_index, src, 'sum')
     timeit(pytorch_spmm, iter, adj, src)
     timeit(pyg_spmm, iter, adj_torch_sparse, src, 'sum')
+    timeit(dgsparse_spmm, iter, dgsparse_adj, src)
 
 
 if __name__ == '__main__':
-    dataset = 'ogbl-collab'
+    dataset = 'ppi'
     feature_size = 64
     device = "cuda"
-    test_gather_scatter(dataset, feature_size, device)
+    with open("benchop_spmm.csv", "w") as file:
+        test_gather_scatter(file, dataset, feature_size, device)
