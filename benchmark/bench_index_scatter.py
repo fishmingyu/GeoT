30e92f6125fdb59c80dcf5a654177bb3d90c918a
fishmingyu <1661342068@qq.com>
2024-03-08 21:19:57 -0800

    [Update] Benchmark

--- benchmark/bench_index_scatter.py
+++ benchmark/bench_index_scatter.py
@@ -38,24 +38,35 @@ def timeit(func, iter, *args, **kwargs):
     end = time.time()
     # return each func's ms
     print(f"{func.__name__} time: {(end - start) / iter * 1000:.3f} ms")
+    return (end - start) / iter * 1000
 
 
-def test_index_scatter(dataset, feature_size, device):
+def test_index_scatter(file, dataset, feature_size, device):
     g = Dataset(dataset, device)
     idx = g.idx
-    print(idx.size())
     src = torch.rand(idx.size(0), feature_size).to(device)
     # benchmark time
     iter = 100
-    timeit(pyg_scatter_reduce, iter, idx, src)
-    timeit(pyg_segment_coo, iter, idx, src)
-    timeit(torch_scatter_reduce, iter, idx, src)
-    timeit(index_scatter_reduce, iter, idx, src)
-    timeit(torch_index_reduce, iter, idx, src)
+    # file is a csv file
+    t1 = timeit(pyg_scatter_reduce, iter, idx, src)
+    t2 = timeit(pyg_segment_coo, iter, idx, src)
+    t3 = timeit(torch_scatter_reduce, iter, idx, src)
+    t4 = timeit(index_scatter_reduce, iter, idx, src)
+    t5 = timeit(torch_index_reduce, iter, idx, src)
+    # :.4f 
+    file.write(f"{t1:.4f},{t2:.4f},{t3:.4f},{t4:.4f},{t5:.4f}")
 
 
 if __name__ == '__main__':
-    dataset = 'amazon_photo'
-    feature_size = 128
+    datasets = ['cora', 'citeseer', 'pubmed', 'amazon_photo', 'ppi', 'yelp', 'ogbn-arxiv', 'ogbl-collab']
+    feature_size = [4, 8, 16, 32, 64, 128]
     device = "cuda"
-    test_index_scatter(dataset, feature_size, device)
+    # write benchmark result to csv file
+    with open("benchop_index_scatter.csv", "w") as file:
+        file.write("dataset,feature_size,pyg_scatter_reduce,pyg_segment_coo,torch_scatter_reduce,index_scatter_reduce,torch_index_reduce\n")
+        for d in datasets:
+            for f in feature_size:
+                print(f"Testing {d} with feature size {f}")
+                file.write(f"{d},{f},")
+                test_index_scatter(file, d, f, device)
+                file.write("\n")
