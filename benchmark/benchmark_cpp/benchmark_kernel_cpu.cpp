e87713bccf08342c7439c381d4586c51cd6214dc
Chen, Xin <xin.chen@intel.com>
2024-03-21 04:20:23 -0400

    [Update] change CMAKE and benchmark_kernel_cpu.cpp

--- benchmark/benchmark_cpp/benchmark_kernel_cpu.cpp
+++ benchmark/benchmark_cpp/benchmark_kernel_cpu.cpp
@@ -4,9 +4,27 @@
 //Version: 1.0
 
 //This file aims to implement CPU code
+
+#include "./include/npy.hpp"
+#include "./include/seg_func.hpp"
+
 std::vector<long> npy_data_loader(const char *filename) {
   std::string file_path(filename);
   npy::npy_data<long> d = npy::read_npy<long>(file_path);
   std::vector<long> data = d.data;
   return data;
+}
+
+int main(int argc, char*argv[])
+{
+  if (argc !=3)
+  {
+    printf("Input: first get the path of sparse matrix, then get the "
+           "feature length of dense matrix\n");
+    exit(1);
+  }
+
+  char *filename = argv[1];
+  
+
 }
