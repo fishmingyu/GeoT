a1bf477e08e57ac7bf4d79d9c22456d1a7c12f7e
Hengjia Yu <hengjia.y@outlook.com>
2024-07-15 17:26:03 -0700

    add gather_weight_scatter pattern-match for GCN

--- test/compile/test_gcn.py
+++ test/compile/test_gcn.py
@@ -1,20 +1,22 @@
 import torch
 from torch.export import export
-from torch_geometric.nn import GCNConv, SAGEConv
+from torch.fx import subgraph_rewriter
+from torch_geometric.nn import GCNConv
 from torch_geometric.testing import (
     onlyFullTest,
     onlyLinux,
     withPackage,
 )
+import geot
 
 if __name__ == '__main__':
     import argparse
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--backward', action='store_true')
     args = parser.parse_args()
 
+    # generate random input
     num_nodes, num_edges = 10_000, 200_000
     x = torch.randn(num_nodes, 64, device=args.device)
     edge_index = torch.randint(num_nodes, (2, num_edges), device=args.device)
@@ -29,55 +31,35 @@ if __name__ == '__main__':
     
     # args: no self loop
     kwargs = {'node_dim': 0}
-    conv = SAGEConv(64, 64, **kwargs).to(args.device)
-    out_conv = conv(x, edge_index)
+    conv = GCNConv(64, 64, **kwargs).to(args.device)
+    
+    # get control output
+    out_gcn = conv(x, edge_index)
     
     # get macros about size
-
     exported = export(conv,(x, edge_index))
     NS = x.size(0)
     FS = x.size(1)
     exp_graph = exported.graph_module
     reduce = 'sum'
     
+    # define pattern and replacement
+    def pattern_weight_scatter(linear, mul_1, cat):
+        select_4 = torch.ops.aten.select.int(cat, 0, 1)
+        select_5 = torch.ops.aten.select.int(cat, 0, 0)
+        index_select = torch.ops.aten.index_select.default(linear, 0, select_5)
+        view_1 = torch.ops.aten.view.default(mul_1, [-1, 1])
+        mul_2 = torch.ops.aten.mul.Tensor(view_1, index_select)
+        new_zeros_1 = torch.ops.aten.new_zeros.default(mul_2, [10000, 64], pin_memory = False)
+        return torch.ops.aten.index_add.default(new_zeros_1, 0, select_4, mul_2)
     
-    def transform(m : torch.fx.GraphModule) -> torch.fx.GraphModule:
-        graph = m.graph
-        for node in graph.nodes:
-            # locate row and col
-            if node.op == 'call_function' and node.target == torch.ops.aten.select.int:
-                if node.args[2] == 0:
-                    var_row = node
-                elif node.args[2] == 1:
-                    var_col = node
-            # locate index_select i.e. x_j = x[row]
-            if node.op == 'call_function' and node.target == torch.ops.aten.index_select.default:
-                if str(node.args[0]) == "x" and node.args[2] == var_row:
-                    var_index_select = node
-                    var_x = node.args[0]   
-            # locate index_add, then replace with gather_scatter 
-            if node.op == 'call_function' and node.target == torch.ops.aten.index_add.default:
-                if node.args[3] == var_index_select:
-                    with graph.inserting_before(node):
-                        new_node = graph.call_function(
-                            torch.ops.geot.gather_scatter, args=(var_row, var_col, var_x, reduce))
-                        node.replace_all_uses_with(new_node)
-                        graph.erase_node(node)          # erase index_add_1
-                    # replace all uses of index_select with x (only used its dtype and device)
-                    for node_replace in graph.nodes:
-                        for n in range(len(node_replace.args)):
-                            if node_replace.args[n] == var_index_select:
-                                args_list = list(node_replace.args) 
-                                args_list[n] = var_x  
-                                node_replace.args = tuple(args_list) 
-                    # erase index_select
-                    graph.erase_node(var_index_select)
-        graph.eliminate_dead_code()
-        graph.lint()
-        m.recompile()
-    
-    print(f'\nBefore:{exp_graph.graph}')
-    transform(exp_graph)
+    def replacement_weight_scatter(linear, mul_1, cat):
+        row = torch.ops.aten.select.int(cat, 0, 0)
+        col = torch.ops.aten.select.int(cat, 0, 1)
+        return torch.ops.geot.gather_weight_scatter(row, col, mul_1, linear, reduce)
+        
+    print(f'\nBefore:{exp_graph.code}')
+    subgraph_rewriter.replace_pattern(exp_graph, pattern_weight_scatter, replacement_weight_scatter)
     print(f'\nAfter:{exp_graph.code}')
     
     compile_exported = torch.compile(exported.module())
@@ -85,6 +67,6 @@ if __name__ == '__main__':
     out_compile = compile_exported(x, edge_index)
     assert torch.allclose(out_replace, out_compile, atol=1e-6)
     
-    diff = torch.abs(out_conv - out_compile).max()
-    print(f'max difference: {diff}')
-    assert torch.allclose(out_conv, out_compile, atol=1e-6)
+    diff = torch.abs(out_gcn - out_compile).max()
+    print(f'max difference with GCN: {diff}')
+    assert torch.allclose(out_gcn, out_compile, atol=1e-6)
---
+++ test/compile/test_sage.py
@@ -0,0 +1,93 @@
+import torch
+from torch.export import export
+from torch_geometric.nn import SAGEConv
+from torch_geometric.testing import (
+    onlyFullTest,
+    onlyLinux,
+    withPackage,
+)
+import geot
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--backward', action='store_true')
+    args = parser.parse_args()
+
+    # generate random input
+    num_nodes, num_edges = 10_000, 200_000
+    x = torch.randn(num_nodes, 64, device=args.device)
+    edge_index = torch.randint(num_nodes, (2, num_edges), device=args.device)
+    
+    # sort edge_index by col
+    _ , indices = torch.sort(edge_index[1, :])
+    edge_index = edge_index[ : , indices]
+    
+    # check if already sorted by col
+    row, col = edge_index
+    assert torch.all(col[:-1] <= col[1:])
+    
+    # args: no self loop
+    kwargs = {'node_dim': 0}
+    conv = SAGEConv(64, 64, **kwargs).to(args.device)
+    
+    # get control output
+    out_sage = conv(x, edge_index)
+    
+    # get macros about size
+    exported = export(conv,(x, edge_index))
+    NS = x.size(0)
+    FS = x.size(1)
+    exp_graph = exported.graph_module
+    reduce = 'sum'
+        
+    # manually replace index_add_ and index_select 
+    # with fused operation (geot.gather_scatter)
+    def transform(m : torch.fx.GraphModule) -> torch.fx.GraphModule:
+        graph = m.graph
+        for node in graph.nodes:
+            # locate row and col
+            var_index_select : torch.fx.Node
+            if node.op == 'call_function' and node.target == torch.ops.aten.select.int:
+                if node.args[2] == 0:
+                    var_row = node
+                elif node.args[2] == 1:
+                    var_col = node
+            # locate index_select i.e. x_j = x[row]
+            if node.op == 'call_function' and node.target == torch.ops.aten.index_select.default:
+                var_index_select = node
+                var_x = node.args[0]   
+            # locate index_add, then replace with gather_scatter 
+            if node.op == 'call_function' and node.target == torch.ops.aten.index_add.default:
+                if node.args[3] == var_index_select:
+                    with graph.inserting_before(node):
+                        new_node = graph.call_function(
+                            torch.ops.geot.gather_scatter, args=(var_row, var_col, var_x, reduce))
+                        node.replace_all_uses_with(new_node)
+                        graph.erase_node(node)          # erase index_add_1
+                    # replace all uses of index_select with x (only used its dtype and device)
+                    for node_replace in graph.nodes:
+                        for n in range(len(node_replace.args)):
+                            if node_replace.args[n] == var_index_select:
+                                args_list = list(node_replace.args) 
+                                args_list[n] = var_x  
+                                node_replace.args = tuple(args_list) 
+                    # erase index_select
+                    graph.erase_node(var_index_select)
+        graph.eliminate_dead_code()
+        graph.lint()
+        m.recompile()
+    
+    print(f'\nBefore:{exp_graph.code}')
+    transform(exp_graph)
+    print(f'\nAfter:{exp_graph.code}')
+    
+    compile_exported = torch.compile(exported.module())
+    out_replace = exported.module()(x, edge_index)
+    out_compile = compile_exported(x, edge_index)
+    assert torch.allclose(out_replace, out_compile, atol=1e-6)
+    
+    diff = torch.abs(out_sage - out_compile).max()
+    print(f'max difference with SAGE: {diff}')
+    assert torch.allclose(out_sage, out_compile, atol=1e-6)
