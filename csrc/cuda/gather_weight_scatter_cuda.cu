6ae17bd1c74d0590f1e496141031cf008164acd0
Hengjia Yu <hengjia.y@outlook.com>
2024-09-30 02:48:01 -0700

    format code

--- csrc/cuda/gather_weight_scatter_cuda.cu
+++ csrc/cuda/gather_weight_scatter_cuda.cu
@@ -1,6 +1,6 @@
+#include "./sddmm_kernel.cuh"
 #include "./wrapper/gather_weight_scatter_base.h"
 #include "./wrapper/gather_weight_scatter_rule.h"
-#include "./sddmm_kernel.cuh"
 #include "header_cuda.h"
 
 void gather_weight_scatter_sorted_dispatch(const at::Tensor &src_index,
@@ -8,13 +8,14 @@ void gather_weight_scatter_sorted_dispatch(const at::Tensor &src_index,
                                            const at::Tensor &weight,
                                            const at::Tensor &src,
                                            const at::Tensor &dst,
-                                           const ReductionType &reduction)
-{
+                                           const ReductionType &reduction) {
   AT_DISPATCH_FLOATING_TYPES(
-      src.scalar_type(), "gather_weight_scatter_sorted", [&]
-      { DISPATCH_REDUCTION_TYPES(reduction, [&]()
-                                 { gather_weight_scatter_sorted_wrapper<scalar_t, reduce>(
-                                       src_index, dst_index, weight, src, dst); }); });
+      src.scalar_type(), "gather_weight_scatter_sorted", [&] {
+        DISPATCH_REDUCTION_TYPES(reduction, [&]() {
+          gather_weight_scatter_sorted_wrapper<scalar_t, reduce>(
+              src_index, dst_index, weight, src, dst);
+        });
+      });
 }
 
 // we assume that the dst_index is already sorted
@@ -23,8 +24,7 @@ at::Tensor gather_weight_scatter_cuda(const at::Tensor &src_index,
                                       const at::Tensor &weight,
                                       const at::Tensor &src,
                                       const at::Tensor &dst,
-                                      const c10::string_view reduce)
-{
+                                      const c10::string_view reduce) {
   TORCH_CHECK(src_index.dim() == dst_index.dim() && src_index.dim() == 1,
               "src_index and dst_index must be 1 dimensional");
   TORCH_CHECK(src.dim() == 2, "src must be 2 dimensional");
@@ -38,12 +38,9 @@ at::Tensor gather_weight_scatter_cuda(const at::Tensor &src_index,
   return dst;
 }
 
-void sddmm_coo_cuda(const at::Tensor &src_index,
-                    const at::Tensor &dst_index,
-                    const at::Tensor &mat_1,
-                    const at::Tensor &mat_2,
-                    at::Tensor &output)
-{
+void sddmm_coo_cuda(const at::Tensor &src_index, const at::Tensor &dst_index,
+                    const at::Tensor &mat_1, const at::Tensor &mat_2,
+                    at::Tensor &output) {
   int D_kcol = mat_1.size(1);   // feature dimension
   int Size = src_index.size(0); // number of edges
   auto col_indices = src_index.data_ptr<int>();
@@ -52,16 +49,14 @@ void sddmm_coo_cuda(const at::Tensor &src_index,
   auto X2 = mat_2.data_ptr<float>();
   auto out = output.data_ptr<float>();
   dim3 gridDim(Size / 16 + (Size & 15), 1, 1);
-  if ((D_kcol % 4) == 0)
-  {
-    sddmm_coo_ebalance_vec4<<<gridDim, dim3(8, 4, 1)>>>(D_kcol, Size, row_indices, col_indices, X1, X2, out);
-  }
-  else if ((D_kcol % 2) == 0)
-  {
-    sddmm_coo_ebalance_vec2<<<gridDim, dim3(16, 4, 1)>>>(D_kcol, Size, row_indices, col_indices, X1, X2, out);
-  }
-  else
-  {
-    sddmm_coo_ebalance_scalar<<<gridDim, dim3(32, 4, 1)>>>(D_kcol, Size, row_indices, col_indices, X1, X2, out);
+  if ((D_kcol % 4) == 0) {
+    sddmm_coo_ebalance_vec4<<<gridDim, dim3(8, 4, 1)>>>(
+        D_kcol, Size, row_indices, col_indices, X1, X2, out);
+  } else if ((D_kcol % 2) == 0) {
+    sddmm_coo_ebalance_vec2<<<gridDim, dim3(16, 4, 1)>>>(
+        D_kcol, Size, row_indices, col_indices, X1, X2, out);
+  } else {
+    sddmm_coo_ebalance_scalar<<<gridDim, dim3(32, 4, 1)>>>(
+        D_kcol, Size, row_indices, col_indices, X1, X2, out);
   }
 }
--- csrc/cuda/sddmm_kernel.cuh
+++ csrc/cuda/sddmm_kernel.cuh
@@ -3,13 +3,11 @@
 __global__ void sddmm_coo_ebalance_vec4(int D_kcols, const int Size,
                                         int *S_cooRowInd, int *S_cooColInd,
                                         float *D1_dnVal, float *D2_dnVal,
-                                        float *O_cooVal)
-{
+                                        float *O_cooVal) {
   int eid = (blockIdx.x << 4) + (threadIdx.y << 2);
   int cid = (threadIdx.x << 2);
 
-  if (blockIdx.x < Size / 16)
-  {
+  if (blockIdx.x < Size / 16) {
     float multi[4] = {0, 0, 0, 0};
     int offset1[4], offset2[4];
     float4 D1tmp[4], D2tmp[4];
@@ -18,22 +16,18 @@ __global__ void sddmm_coo_ebalance_vec4(int D_kcols, const int Size,
     selfMulConst4<int>(offset1, D_kcols);
     selfMulConst4<int>(offset2, D_kcols);
 
-    for (int i = 0; i < (D_kcols >> 5); i++)
-    {
+    for (int i = 0; i < (D_kcols >> 5); i++) {
       Load4<float4, float>(D1tmp, D1_dnVal, offset1, cid);
       Load4<float4, float>(D2tmp, D2_dnVal, offset2, cid);
       vec4Dot4<float4, float>(multi, D1tmp, D2tmp);
       cid += 32;
     }
     int res = D_kcols & 31;
-    if (res)
-    {
+    if (res) {
       int cid2 = threadIdx.x + D_kcols - res;
       float D1[4] = {0, 0, 0, 0}, D2[4] = {0, 0, 0, 0};
-      for (int i = 0; i < res / 8 + 1; i++)
-      {
-        if (i * 8 + threadIdx.x < res)
-        {
+      for (int i = 0; i < res / 8 + 1; i++) {
+        if (i * 8 + threadIdx.x < res) {
           Load4<float, float>(D1, D1_dnVal, offset1, cid2);
           Load4<float, float>(D2, D2_dnVal, offset2, cid2);
           Dot4<float>(multi, D1, D2);
@@ -42,12 +36,10 @@ __global__ void sddmm_coo_ebalance_vec4(int D_kcols, const int Size,
       }
     }
     AllReduce4<float>(multi, 4, 32);
-    if (threadIdx.x == 0)
-    {
+    if (threadIdx.x == 0) {
       Store<float4, float>(O_cooVal, multi, eid);
     }
-  }
-  else // Dynamic parrallel?
+  } else // Dynamic parrallel?
   {
     eid = Size - (Size & 15) + (blockIdx.x - (Size / 16));
     int offset1 = S_cooRowInd[eid] * D_kcols;
@@ -55,8 +47,7 @@ __global__ void sddmm_coo_ebalance_vec4(int D_kcols, const int Size,
     float multi = 0;
     int off1 = cid = threadIdx.x + (threadIdx.y << 3);
     float D1tmp0, D2tmp0;
-    for (int cc = 0; cc < (D_kcols >> 5); cc++)
-    {
+    for (int cc = 0; cc < (D_kcols >> 5); cc++) {
       D1tmp0 = D1_dnVal[offset1 + cid];
       D2tmp0 = D2_dnVal[offset2 + cid];
       multi += D1tmp0 * D2tmp0;
@@ -64,21 +55,17 @@ __global__ void sddmm_coo_ebalance_vec4(int D_kcols, const int Size,
     }
     int res = D_kcols & 31;
     D1tmp0 = D2tmp0 = 0;
-    if (res)
-    {
-      if (off1 < res)
-      {
+    if (res) {
+      if (off1 < res) {
         D1tmp0 = D1_dnVal[offset1 + cid];
         D2tmp0 = D2_dnVal[offset2 + cid];
       }
       multi += D1tmp0 * D2tmp0;
     }
-    for (int stride = 16; stride > 0; stride >>= 1)
-    {
+    for (int stride = 16; stride > 0; stride >>= 1) {
       multi += __shfl_xor_sync(0xffffffff, multi, stride, 32);
     }
-    if (threadIdx.x == 0 && threadIdx.y == 0)
-    {
+    if (threadIdx.x == 0 && threadIdx.y == 0) {
       O_cooVal[eid] = multi;
     }
   }
@@ -87,13 +74,11 @@ __global__ void sddmm_coo_ebalance_vec4(int D_kcols, const int Size,
 __global__ void sddmm_coo_ebalance_vec2(int D_kcols, const int Size,
                                         int *S_cooRowInd, int *S_cooColInd,
                                         float *D1_dnVal, float *D2_dnVal,
-                                        float *O_cooVal)
-{
+                                        float *O_cooVal) {
   int eid = (blockIdx.x << 4) + (threadIdx.y << 2);
   int cid = threadIdx.x << 1;
 
-  if (blockIdx.x < Size / 16)
-  {
+  if (blockIdx.x < Size / 16) {
     float multi[4] = {0, 0, 0, 0};
     int offset1[4], offset2[4];
     float2 D1tmp[4], D2tmp[4];
@@ -102,22 +87,18 @@ __global__ void sddmm_coo_ebalance_vec2(int D_kcols, const int Size,
     selfMulConst4<int>(offset1, D_kcols);
     selfMulConst4<int>(offset2, D_kcols);
 
-    for (int i = 0; i < (D_kcols >> 5); i++)
-    {
+    for (int i = 0; i < (D_kcols >> 5); i++) {
       Load4<float2, float>(D1tmp, D1_dnVal, offset1, cid);
       Load4<float2, float>(D2tmp, D2_dnVal, offset2, cid);
       vec2Dot4<float2>(multi, D1tmp, D2tmp);
       cid += 32;
     }
     int res = D_kcols & 31;
-    if (res)
-    {
+    if (res) {
       int cid2 = threadIdx.x + D_kcols - res;
       float D1[4] = {0, 0, 0, 0}, D2[4] = {0, 0, 0, 0};
-      for (int i = 0; i < (res >> 4) + 1; i++)
-      {
-        if ((i << 4) + threadIdx.x < res)
-        {
+      for (int i = 0; i < (res >> 4) + 1; i++) {
+        if ((i << 4) + threadIdx.x < res) {
           Load4<float, float>(D1, D1_dnVal, offset1, cid2);
           Load4<float, float>(D2, D2_dnVal, offset2, cid2);
           Dot4<float>(multi, D1, D2);
@@ -126,12 +107,10 @@ __global__ void sddmm_coo_ebalance_vec2(int D_kcols, const int Size,
       }
     }
     AllReduce4<float>(multi, 8, 32);
-    if (threadIdx.x == 0)
-    {
+    if (threadIdx.x == 0) {
       Store<float4, float>(O_cooVal, multi, eid);
     }
-  }
-  else // Dynamic parrallel?
+  } else // Dynamic parrallel?
   {
     eid = Size - (Size & 15) + (blockIdx.x - (Size / 16));
     int offset1 = S_cooRowInd[eid] * D_kcols;
@@ -139,8 +118,7 @@ __global__ void sddmm_coo_ebalance_vec2(int D_kcols, const int Size,
     float multi = 0;
     int off1 = cid = (threadIdx.y << 4) + threadIdx.x;
     float D1tmp0, D2tmp0;
-    for (int cc = 0; cc < (D_kcols >> 5); cc++)
-    {
+    for (int cc = 0; cc < (D_kcols >> 5); cc++) {
       D1tmp0 = D1_dnVal[offset1 + cid];
       D2tmp0 = D2_dnVal[offset2 + cid];
       multi += D1tmp0 * D2tmp0;
@@ -148,21 +126,17 @@ __global__ void sddmm_coo_ebalance_vec2(int D_kcols, const int Size,
     }
     int res = D_kcols & 31;
     D1tmp0 = D2tmp0 = 0;
-    if (res)
-    {
-      if (off1 < res)
-      {
+    if (res) {
+      if (off1 < res) {
         D1tmp0 = D1_dnVal[offset1 + cid];
         D2tmp0 = D2_dnVal[offset2 + cid];
       }
       multi += D1tmp0 * D2tmp0;
     }
-    for (int stride = 16; stride > 0; stride >>= 1)
-    {
+    for (int stride = 16; stride > 0; stride >>= 1) {
       multi += __shfl_xor_sync(0xffffffff, multi, stride, 32);
     }
-    if (threadIdx.x == 0 && threadIdx.y == 0)
-    {
+    if (threadIdx.x == 0 && threadIdx.y == 0) {
       O_cooVal[eid] = multi;
     }
   }
@@ -171,13 +145,11 @@ __global__ void sddmm_coo_ebalance_vec2(int D_kcols, const int Size,
 __global__ void sddmm_coo_ebalance_scalar(int D_kcols, const int Size,
                                           int *S_cooRowInd, int *S_cooColInd,
                                           float *D1_dnVal, float *D2_dnVal,
-                                          float *O_cooVal)
-{
+                                          float *O_cooVal) {
   int eid = (blockIdx.x << 4) + (threadIdx.y << 2);
   int cid = threadIdx.x;
 
-  if (blockIdx.x < Size / 16)
-  {
+  if (blockIdx.x < Size / 16) {
     float multi[4] = {0, 0, 0, 0};
     int offset1[4], offset2[4];
     float D1tmp[4], D2tmp[4];
@@ -186,31 +158,26 @@ __global__ void sddmm_coo_ebalance_scalar(int D_kcols, const int Size,
     selfMulConst4<int>(offset1, D_kcols);
     selfMulConst4<int>(offset2, D_kcols);
 
-    for (int i = 0; i < (D_kcols >> 5); i++)
-    {
+    for (int i = 0; i < (D_kcols >> 5); i++) {
       Load4<float, float>(D1tmp, D1_dnVal, offset1, cid);
       Load4<float, float>(D2tmp, D2_dnVal, offset2, cid);
       Dot4<float>(multi, D1tmp, D2tmp);
       cid += 32;
     }
     int res = D_kcols & 31;
-    if (res)
-    {
+    if (res) {
       float D1[4] = {0, 0, 0, 0}, D2[4] = {0, 0, 0, 0};
-      if (threadIdx.x < res)
-      {
+      if (threadIdx.x < res) {
         Load4<float, float>(D1, D1_dnVal, offset1, cid);
         Load4<float, float>(D2, D2_dnVal, offset2, cid);
         Dot4<float>(multi, D1, D2);
       }
     }
     AllReduce4<float>(multi, 16, 32);
-    if (threadIdx.x == 0)
-    {
+    if (threadIdx.x == 0) {
       Store<float4, float>(O_cooVal, multi, eid);
     }
-  }
-  else // Dynamic parrallel?
+  } else // Dynamic parrallel?
   {
     eid = Size - (Size & 15) + (blockIdx.x - (Size / 16));
     int offset1 = S_cooRowInd[eid] * D_kcols;
@@ -218,8 +185,7 @@ __global__ void sddmm_coo_ebalance_scalar(int D_kcols, const int Size,
     float multi = 0;
     int off1 = cid = threadIdx.x;
     float D1tmp0, D2tmp0;
-    for (int cc = 0; cc < (D_kcols >> 5); cc++)
-    {
+    for (int cc = 0; cc < (D_kcols >> 5); cc++) {
       D1tmp0 = D1_dnVal[offset1 + cid];
       D2tmp0 = D2_dnVal[offset2 + cid];
       multi += D1tmp0 * D2tmp0;
@@ -227,21 +193,17 @@ __global__ void sddmm_coo_ebalance_scalar(int D_kcols, const int Size,
     }
     int res = D_kcols & 31;
     D1tmp0 = D2tmp0 = 0;
-    if (res)
-    {
-      if (off1 < res)
-      {
+    if (res) {
+      if (off1 < res) {
         D1tmp0 = D1_dnVal[offset1 + cid];
         D2tmp0 = D2_dnVal[offset2 + cid];
       }
       multi += D1tmp0 * D2tmp0;
     }
-    for (int stride = 16; stride > 0; stride >>= 1)
-    {
+    for (int stride = 16; stride > 0; stride >>= 1) {
       multi += __shfl_xor_sync(0xffffffff, multi, stride, 32);
     }
-    if (threadIdx.x == 0 && threadIdx.y == 0)
-    {
+    if (threadIdx.x == 0 && threadIdx.y == 0) {
       O_cooVal[eid] = multi;
     }
   }
